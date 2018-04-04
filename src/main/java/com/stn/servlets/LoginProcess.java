dfa241b84e381999ffb097e26fcfdfa4e881c965
MaD421 <37409907+MaD421@users.noreply.github.com>
2018-04-04 13:44:00 +0300

    g
    g

--- src/main/java/com/stn/servlets/LoginProcess.java
+++ src/main/java/com/stn/servlets/LoginProcess.java
@@ -20,6 +20,29 @@ import java.util.Arrays;
 @WebServlet("/LoginProcess")
 public class LoginProcess extends HttpServlet {
 
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR" };
+
+    public static String getClientIpAddress(HttpServletRequest request) {
+        for (String header : IP_HEADER_CANDIDATES) {
+            String ip = request.getHeader(header);
+            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                return ip;
+            }
+        }
+        return request.getRemoteAddr();
+    }
+
     private void updateAttempts(String ip) {
 
         PreparedStatement preparedStatement = null;
@@ -97,12 +120,12 @@ public class LoginProcess extends HttpServlet {
                     } else {
                         error = "Invalid username or password!";
                         url = "login.jsp";
-                        this.updateAttempts(IPHelper.getClientIpAddress(request));
+                        this.updateAttempts(this.getClientIpAddress(request));
                     }
                 } else {
                     error = "Invalid username or password!";
                     url = "login.jsp";
-                    this.updateAttempts(IPHelper.getClientIpAddress(request));
+                    this.updateAttempts(this.getClientIpAddress(request));
                 }
 
             } catch (ClassNotFoundException | SQLException | NoSuchAlgorithmException e) {
