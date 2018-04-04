ae5d02ce62218f3c01ceeec3c52a53d74e682aa7
MaD421 <37409907+MaD421@users.noreply.github.com>
2018-04-04 19:13:03 +0300

    g
    g

---
+++ db (nulled).sql
@@ -0,0 +1,45 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+03:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stn_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_logins`
+--
+
+CREATE TABLE `failed_logins` (
+  `Id` int(11) NOT NULL,
+  `Ip` varchar(30) NOT NULL,
+  `Attempts` int(2) NOT NULL,
+  `ExpireDate` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(10) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(80) NOT NULL,
+  `Salt` varbinary(120) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(30) NOT NULL,
+  `JoinDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `LastSeen` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
