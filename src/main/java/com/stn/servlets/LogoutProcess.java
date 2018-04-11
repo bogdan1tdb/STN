9eb2a109a0db64de628c3fc3e41bfd45c695e730
MaD421 <37409907+MaD421@users.noreply.github.com>
2018-04-11 19:34:50 +0300

    g
    g

--- src/main/java/com/stn/servlets/LogoutProcess.java
+++ src/main/java/com/stn/servlets/LogoutProcess.java
@@ -15,9 +15,9 @@ public class LogoutProcess extends HttpServlet {
         for (Cookie cookie : request.getCookies()) {
             cookie.setValue("");
             cookie.setMaxAge(0);
-            cookie.setPath("/");
             response.addCookie(cookie);
         }
+
         session.invalidate();
 
         response.sendRedirect(url);
