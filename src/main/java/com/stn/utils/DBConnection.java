f29e0850530331fe31bb439b91acccbf7edf89e1
MaD421 <37409907+MaD421@users.noreply.github.com>
2018-04-18 15:00:27 +0300

    g
    g

--- src/main/java/com/stn/utils/DBConnection.java
+++ src/main/java/com/stn/utils/DBConnection.java
@@ -19,16 +19,10 @@ public class DBConnection {
 
     //Se utilizeaza pentru construirea unui query
     public DBConnection() {
-        /*this.dbHost = "mysql";
+        this.dbHost = "165.227.172.85";
         this.dbName = "stn_db";
-        this.dbUser = "user_stn";
-        this.dbPassword = "stnbaca123*";
-        this.dbPort = "3306";*/
-
-        this.dbHost = "johnny.heliohost.org";
-        this.dbName = "mad_stn";
-        this.dbUser = "mad_stnuser";
-        this.dbPassword = "super767&&";
+        this.dbUser = "MaD";
+        this.dbPassword = "super123#";
         this.dbPort = "3306";
         this.preparedStatement = null;
         this.connection = null;
